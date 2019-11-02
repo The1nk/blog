542a347419c9d01a0000c5bf62b77626d56b31b0
adam <me@adamdavis.dev>
2019-11-01 22:47:42 -0400

    Will now serve over both http and htpps

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,2 +1,2 @@
-docker build . -t cloud.canister.io:5000/the1nk/blog:5
-docker push cloud.canister.io:5000/the1nk/blog:5
+docker build . -t cloud.canister.io:5000/the1nk/blog:9
+docker push cloud.canister.io:5000/the1nk/blog:9
