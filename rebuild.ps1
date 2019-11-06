9fa1be2eaf62653bd211555b9856ad9ace1ba1fa
adam <me@adamdavis.dev>
2019-11-06 16:14:32 -0500

    Update TSC-DD Client

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,2 +1,2 @@
-docker build . -t cloud.canister.io:5000/the1nk/blog:23
-docker push cloud.canister.io:5000/the1nk/blog:23
+docker build . -t cloud.canister.io:5000/the1nk/blog:24
+docker push cloud.canister.io:5000/the1nk/blog:24
