f0b12c01859be56a070ee031e10d3c105fbfc82d
adam <me@adamdavis.dev>
2019-11-07 15:10:05 -0500

    Deploy 1.0.0.18

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,2 +1,2 @@
-docker build . -t cloud.canister.io:5000/the1nk/blog:25
-docker push cloud.canister.io:5000/the1nk/blog:25
+docker build . -t cloud.canister.io:5000/the1nk/blog:26
+docker push cloud.canister.io:5000/the1nk/blog:26
