9e3bb36c9486818bff5656080abbd8fb9158466f
adam <me@adamdavis.dev>
2019-12-29 16:37:46 -0500

    Increment version

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,3 +1,3 @@
 vuepress build site
-docker build . -t cloud.canister.io:5000/the1nk/blog:31
-docker push cloud.canister.io:5000/the1nk/blog:31
+docker build . -t cloud.canister.io:5000/the1nk/blog:32
+docker push cloud.canister.io:5000/the1nk/blog:32
